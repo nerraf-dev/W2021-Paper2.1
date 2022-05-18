d3b9ae72f84b41c061a9325463b746ae25d06e4f
nerraf-dev <87069314+nerraf-dev@users.noreply.github.com>
2022-05-18 19:33:59 +0000

    task 1 complete [?]

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 from datetime import datetime
+import time
 
+"""
+Task 1 – at the start of the day
+Write a program to set up arrays to record the following for each hour:
+• whether a squash court is booked or available
+• the name of the guest
+• the mobile phone number of the guest
+• the unique 4-digit code for the booking.
+Set up a screen to display the court availability at the start of the day.
+"""
+NUM_OF_COURTS = 8
 #Set Arrays
 times = ['08:00', '09:00', '10:00', '11:00', '12:00', '13:00', '14:00', '15:00', '16:00', '17:00'] 
 #index 0 = 08:00, 1 = 09:00 etc
@@ -9,6 +20,7 @@ mobile = ['', '', '', '', '', '', '', '', '', '']
 code = ['', '', '', '', '', '', '', '', '', '']
 
 # Create array of above arrays. courts[0] = Court #1, [1] = #2 etc
+#courts[n] = [0 - booked],[1 - name],[2 - mobile],[3 - code]
 courts = []
 for i in range(0,8):
   courts.append([booked, name, mobile, code])
@@ -34,19 +46,37 @@ dayTime = {
   "minute": date.strftime("%M")
 }
 
-
-print("""*** WELCOME TO BSM HOLIDAY PARK ***
- --- SQUASH COURT BOOKING SYSTEM ---
-      """)
-print(f"Today is {dayTime['day']} {date.day} {date.strftime('%B')} {date.year}")
-print(f"The time is {dayTime['hour']}:{dayTime['minute']}\n")
-
-availMsg = (f"""The following times are available:\n\n"""
-f"""Opt  | Time    |  #Courts """)
-print(availMsg)
-print("-"*30)
-for i in range(0, len(times)):
-  if booked[i] == False:
-    a = i+1
-    print(f"{a if a == 10 else '0'+str(a)}   | {times[i]}   | num")
-
+runBookings = True
+while runBookings:
+  print("""**** WELCOME TO BSM HOLIDAY PARK ****
+   --- SQUASH COURT BOOKING SYSTEM ---
+        """)
+  print(f"Today is {dayTime['day']} {date.day} {date.strftime('%B')} {date.year}")
+  print(f"The time is {dayTime['hour']}:{dayTime['minute']}\n")
+  
+  availMsg = (f"""The following times are available:\n\n"""
+  f"""Opt  | Time    |  #Courts """)
+  print(availMsg)
+  print("-"*30)
+  for i in range(0, NUM_OF_COURTS):    #O/L: iterate courts
+    free = 0
+    for j in range(0,len(times)):  #I/L: 
+      if courts[i][0][j] == False:
+        a = i+1
+        free += 1
+    print(f"{a if a == 10 else ' '+str(a)}   | {times[i]}   | {free}")
+      
+    # if booked[i] == False:
+    #   a = i+1
+    #   print(f"{a if a == 10 else ' '+str(a)}   | {times[i]}   | num")
+  
+  print()
+  print("To make a booking enter the option number for your chosen time:")
+  command = input(">> ")
+  if command.upper() == "Q":
+    runBookings = False
+    print("*** QUITTING ***")    #Just to jazz it up a little!
+    time.sleep(1)
+    print("*** SHUTDOWN COMPLETE ***")
+  else:
+    pass    #Pass is used to help run empty loops and selection blocks
